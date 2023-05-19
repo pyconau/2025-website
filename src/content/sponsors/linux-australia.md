e2bc468c66597d5c3367094836b42801ef15cbdc
Daisy Leigh Brenecki <daisy@daisy.wtf>
2023-05-19 17:59:20 +1000

    move some sponsor stuff around

---
+++ src/content/sponsors/linux-australia.md
@@ -0,0 +1,6 @@
+---
+name: Linux Australia
+logo: linux-australia.svg
+url: https://linux.org.au
+tier: auspice
+---
---
+++ src/util.ts
@@ -0,0 +1,24 @@
+import { getCollection, CollectionEntry } from "astro:content"
+
+type SponsorsByTier = {
+  tier: "platinum" | "auspice"
+  sponsors: CollectionEntry<"sponsors">[]
+}[]
+
+const data: SponsorsByTier = [
+  { tier: "platinum", sponsors: [] },
+  { tier: "auspice", sponsors: [] },
+]
+let dataIsPopulated = false
+
+export async function getSponsorsByTier(): Promise<SponsorsByTier> {
+  if (dataIsPopulated) return data
+  const allSponsors = await getCollection("sponsors")
+  for (const tierObj of data) {
+    tierObj.sponsors = allSponsors.filter(
+      (sponsor) => sponsor.data.tier === tierObj.tier,
+    )
+  }
+  dataIsPopulated = true
+  return data
+}
